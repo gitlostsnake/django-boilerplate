37625b2b7faedad6544654ab1ae4f3775e98166f
= <=>
2021-11-08 13:52:39 +0000

    Added in pipfile and piplock file using pipenv 'lock' command and 'check'

--- home/services.py
+++ home/services.py
@@ -1,5 +1,11 @@
+from .models import *
 
+def contact_us_message_create(name, email, subject, message):
+    m = ContactUsMessage()
+    m.name = name
+    m.email = email
+    m.subject = subject
+    m.message = message
+    m.save()
 
-def contact_us_message_save(name, email, subject, message):
-
-    pass
+    
